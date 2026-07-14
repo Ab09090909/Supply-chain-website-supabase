dcf1760d8f5bff86552ceb726a44da8f94d9d9a3
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-14 18:33:39 +0300

    Add files via upload

---
+++ auth/__init__.py
@@ -0,0 +1,5 @@
+"""Auth package - login, signup, password reset, session, verification.
+
+Submodules are imported lazily (on first use) to prevent one broken import
+from crashing the entire app.
+"""
