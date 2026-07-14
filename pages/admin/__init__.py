08c20285b1c90c2a63b1e871d68e0b683bf83b8b
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-14 18:14:10 +0300

    Update __init__.py

--- pages/admin/__init__.py
+++ pages/admin/__init__.py
@@ -1,12 +1,1 @@
-"""Admin pages."""
-from .dashboard import render_admin_dashboard
-from .users import render_admin_users
-from .fraud import render_admin_fraud
-from .profile import render_admin_profile
 
-__all__ = [
-    "render_admin_dashboard",
-    "render_admin_users",
-    "render_admin_fraud",
-    "render_admin_profile",
-]
