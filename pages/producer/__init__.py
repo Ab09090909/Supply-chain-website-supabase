83f14687f204bac84f00df2b35c3b9f84524f6b5
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-14 13:08:28 +0300

    Update __init__.py

--- pages/producer/__init__.py
+++ pages/producer/__init__.py
@@ -3,10 +3,12 @@ from .dashboard import render_producer_dashboard
 from .inventory import render_producer_inventory
 from .orders import render_producer_orders
 from .profile import render_producer_profile
+from .merchant_match import render_producer_merchant_match
 
 __all__ = [
     "render_producer_dashboard",
     "render_producer_inventory",
     "render_producer_orders",
     "render_producer_profile",
+    "render_producer_merchant_match",
 ]
