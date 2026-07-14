e74d3c0f445a6975c4235ac4f8555f768824b7b8
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-14 09:53:10 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,33 @@
+"""Utils package - shared UI helpers, constants, formatting, storage."""
+from .ui import (
+    page_header,
+    role_badge,
+    show_success_toast,
+    show_error_message,
+    show_info_message,
+    metric_card,
+    stat_card,
+    sidebar_user_card,
+)
+from .constants import ROLE_OPTIONS, ROLE_DESCRIPTIONS, ROLE_COLORS
+from .helpers import format_currency, format_datetime, generate_order_number
+from .storage import upload_image, render_image_uploader
 
+__all__ = [
+    "page_header",
+    "role_badge",
+    "show_success_toast",
+    "show_error_message",
+    "show_info_message",
+    "metric_card",
+    "stat_card",
+    "sidebar_user_card",
+    "ROLE_OPTIONS",
+    "ROLE_DESCRIPTIONS",
+    "ROLE_COLORS",
+    "format_currency",
+    "format_datetime",
+    "generate_order_number",
+    "upload_image",
+    "render_image_uploader",
+]
