eab64a3b0a3b7c0058e2ca1008121bcedfabde96
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-14 18:14:54 +0300

    Update __init__.py

--- pages/customer/__init__.py
+++ pages/customer/__init__.py
@@ -1,12 +1,1 @@
-"""Customer pages."""
-from .marketplace import render_customer_marketplace
-from .cart import render_customer_cart
-from .orders import render_customer_orders
-from .profile import render_customer_profile
 
-__all__ = [
-    "render_customer_marketplace",
-    "render_customer_cart",
-    "render_customer_orders",
-    "render_customer_profile",
-]
