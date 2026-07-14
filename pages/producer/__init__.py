529655cc40eb5a03d4916bf865c7a911db54737c
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-14 18:15:29 +0300

    Update __init__.py

--- pages/producer/__init__.py
+++ pages/producer/__init__.py
@@ -1,14 +1,1 @@
-"""Producer pages."""
-from .dashboard import render_producer_dashboard
-from .inventory import render_producer_inventory
-from .orders import render_producer_orders
-from .profile import render_producer_profile
-from .merchant_match import render_producer_merchant_match
 
-__all__ = [
-    "render_producer_dashboard",
-    "render_producer_inventory",
-    "render_producer_orders",
-    "render_producer_profile",
-    "render_producer_merchant_match",
-]
