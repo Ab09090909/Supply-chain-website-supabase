4882b1c4853c32466e3382ac0ef601df6c5a4c83
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-14 11:54:46 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-"""Utils package - shared UI helpers, constants, formatting, storage."""
+"""Utils package - shared UI helpers, constants, formatting, storage, theme."""
 from .ui import (
     page_header,
     role_badge,
@@ -9,9 +9,14 @@ from .ui import (
     stat_card,
     sidebar_user_card,
 )
-from .constants import ROLE_OPTIONS, ROLE_DESCRIPTIONS, ROLE_COLORS
-from .helpers import format_currency, format_datetime, generate_order_number
+from .constants import (
+    ROLE_OPTIONS, ROLE_DESCRIPTIONS, ROLE_COLORS,
+    UNIT_OPTIONS, UNIT_SHORT, QUALITY_GRADES, CERTIFICATION_OPTIONS,
+    PAYMENT_TERMS, PRODUCT_CATEGORIES, CURRENCY_CODE, CURRENCY_SYMBOL,
+)
+from .helpers import format_currency, format_datetime, generate_order_number, format_unit
 from .storage import upload_image, render_image_uploader
+from .theme import init_theme, render_theme_toggle, apply_theme_css
 
 __all__ = [
     "page_header",
@@ -25,9 +30,21 @@ __all__ = [
     "ROLE_OPTIONS",
     "ROLE_DESCRIPTIONS",
     "ROLE_COLORS",
+    "UNIT_OPTIONS",
+    "UNIT_SHORT",
+    "QUALITY_GRADES",
+    "CERTIFICATION_OPTIONS",
+    "PAYMENT_TERMS",
+    "PRODUCT_CATEGORIES",
+    "CURRENCY_CODE",
+    "CURRENCY_SYMBOL",
     "format_currency",
     "format_datetime",
+    "format_unit",
     "generate_order_number",
     "upload_image",
     "render_image_uploader",
+    "init_theme",
+    "render_theme_toggle",
+    "apply_theme_css",
 ]
